1ec03d833b48ff03e8c0b249b9a77cb6e860dda9
rj-mo <73202188+rj-mo@users.noreply.github.com>
2020-10-29 15:38:38 +0000

    working on dynamics

--- dead/copter/copter.py
+++ dead/copter/copter.py
@@ -1,9 +1,16 @@
+from pyquaternion import Quaternion
+
+
 class DeadCopter:
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         # system parameters
         self.mass = 1.5
+        print(kwargs)
         # include the system state
 
-    def dynamics(self, control_actions):
-        pass
+    def dynamics(self, control_actions, state):
+        omega = state[4:7]
+        q = Quaternion(state[0:4])
+        w_quat = Quaternion([0] + omega)
+        return [0.5*q*w_quat, ]
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import dead
 
-copter = dead.copter.DeadCopter()
+copter = dead.copter.DeadCopter(asdf=1)
 print(f"copter mass: {copter.mass}")
+result = copter.dynamics([1, 2, 3], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(result)
