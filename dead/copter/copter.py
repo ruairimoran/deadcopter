af1a6caeeda1c103a549a05af03798ca7c2c1ecd
Pantelis Sopasakis <p.sopasakis@gmail.com>
2020-10-29 12:28:44 +0000

    project structure

---
+++ dead/copter/copter.py
@@ -0,0 +1,9 @@
+class DeadCopter:
+
+    def __init__(self):
+        # system parameters
+        self.mass = 1.5
+        # include the system state
+
+    def dynamics(self, control_actions):
+        pass
