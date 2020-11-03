a2a8a736a95720a4e978821c52fa059cfbf887db
Pantelis Sopasakis <p.sopasakis@gmail.com>
2020-11-03 22:39:34 +0000

    simulation in main.py

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ import matplotlib.pyplot as plt
 copter = dead.copter.DeadCopter(mass=1.3, arm_length=0.8, K_v=100)
 copter.mass = 2
 
-x_cache = []
-for k in range(200):
-    x = copter.quaternion
-    x_norm = np.linalg.norm(x)
-    x_cache += [copter.euler_angles()]
-    copter.fly_simulate([0, -np.sin(k/50)/100, 0], 0.01)
+t_sampling = 0.01  # 10ms
+t_simulation = 2   # simulation time (in s)
+num_simulation_points = np.int(np.ceil(t_simulation / t_sampling))
 
-plt.plot(x_cache)
+black_box_euler = copter.euler_angles()
+
+for k in range(num_simulation_points):
+    black_box_euler = np.vstack((black_box_euler, copter.euler_angles()))
+    copter.fly_simulate([0, np.sin(k/50)/100, 0], 0.01)
+
+
+t_span = np.arange(num_simulation_points+1) * t_sampling
+plt.plot(t_span, black_box_euler)
 plt.show()
