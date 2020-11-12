5f5facd9a10f6db8e807dbfd79b41473cd82e1c4
Pantelis Sopasakis <p.sopasakis@gmail.com>
2020-11-12 19:51:32 +0000

    implemeted linearisation half of it

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import dead
 import numpy as np
 import matplotlib.pyplot as plt
-
+import control as C
 
 copter = dead.copter.DeadCopter(mass=1.3, arm_length=0.8, K_v=100)
+
+a, b = copter.linearisation()
+print(a)
+continuous_time_system = C.ss(a, b, np.eye(9), 0)
+discrete_system = C.c2d(continuous_time_system, Ts=0.1)
+print(discrete_system)
+
+Ad = discrete_system.A
+Bd = discrete_system.B
+
 
 t_sampling = 0.01  # 10ms
 t_simulation = 10   # simulation time (in s)
