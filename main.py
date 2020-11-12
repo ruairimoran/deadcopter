5ca24991286e0bef219cc9bf2ad8831c59e86607
rj-mo <73202188+rj-mo@users.noreply.github.com>
2020-11-12 19:56:40 +0000

    nothing

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ linear_black_box_euler = copter.euler_angles()
 
 for k in range(num_simulation_points):
     black_box_euler = np.vstack((black_box_euler, copter.euler_angles()))
-    copter.fly_simulate([0.001, 0, 0], 0.01)  # np.sin(k/50)/100
+    copter.fly_simulate([0.001, 0.001, 0], 0.01)  # np.sin(k/50)/100
 
 copter.state = np.array([1] + [0] * 9)  # reset state
 
 for k in range(num_simulation_points):
     linear_black_box_euler = np.vstack((linear_black_box_euler, copter.euler_angles()))
-    copter.linear_fly_simulate([0.001, 0, 0], 0.01)  # np.sin(k/50)/100
+    copter.linear_fly_simulate([0.001, 0.001, 0], 0.01)  # np.sin(k/50)/100
 
 t_span = np.arange(num_simulation_points+1) * t_sampling
 plt.plot(t_span, black_box_euler)
