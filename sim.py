2548c8ce1422d94504b126fec58714b65a7df3e1
rj-mo <73202188+rj-mo@users.noreply.github.com>
2021-04-09 15:53:22 +0100

    working simulator with noise

--- sim.py
+++ sim.py
@@ -12,7 +12,7 @@ copter = dead.copter.copter.DeadCopter(disturbance_level=1e-1,
 # simulator intialisation
 sim = dead.copter.simulator.Simulator(t_simulation=1,
                                       t_sampling=1/125,
-                                      measurement_noise_multiplier=1e-7)
+                                      measurement_noise_multiplier=1e-4)
 
 # System design
 Ad, Bd, Cd, K, L, G = sim.system_design(copter)
@@ -21,6 +21,6 @@ print(f"K = {K} \n"
       f"L = {L} \n"
       f"h = {h}")
 
-euler_state_cache, euler_state_hat_cache, control_action_cache, state_cache, state_hat_cache = sim.simulate(copter)
+sim_euler_state_cache, sim_euler_state_hat_cache, sim_control_action_cache, sim_state_cache, sim_state_hat_cache = sim.simulate(copter)
 
-sim.plot_all(euler_state_cache, euler_state_hat_cache, control_action_cache, state_cache, state_hat_cache)
+sim.plot_all(sim_euler_state_cache, sim_euler_state_hat_cache, sim_control_action_cache, sim_state_cache, sim_state_hat_cache)
